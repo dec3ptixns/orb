1d8fc55a78cf27b3e58f18c892e771dec0adeddf
dec3ptixns <129118992+dec3ptixns@users.noreply.github.com>
2023-03-28 22:25:36 +0100

    v1.0.3

--- revamp.js
+++ revamp.js
@@ -2,6 +2,7 @@ const screenMainMenu = document.getElementById('screen_main_menu');
 if (screenMainMenu) {
   const mainMenuRow2 = document.createElement('div');
   mainMenuRow2.classList.add('main_menu_row2');
+  mainMenuRow2.display = 'flex';
   screenMainMenu.appendChild(mainMenuRow2);
 
   const dec3ptionsBtn = document.createElement('div');
@@ -13,42 +14,43 @@ if (screenMainMenu) {
 
 $("#dec3ptions_btn").click(function() { state.set("dec3ptions") })
 
-/*const screenDec3ptions = document.createElement('div');
+const screenDec3ptions = document.createElement('div');
 screenDec3ptions.id = 'screen_dec3ptions';
+screenDec3ptions.style = 'visibility: visible; display: none;';
 screenDec3ptions.classList.add('screen');
 document.body.appendChild(screenDec3ptions);
-*/
-document.getElementById('screen_main_menu').insertAdjacentHTML('beforeEnd',
+
+document.getElementById('screen_dec3ptions').insertAdjacentHTML('beforeEnd',
     //'<div class="screen" id="screen_exploits" style="visibility: visible;"><div class="main_menu_row"><div class="button main_menu_btn" id="exploits_btn" style="color:#7600ff; background:#000000">DEC3PTIXNS HXB</div></div></div>' +
     //'<div class="screen" id="screen_dec3ptions" style="visibility: visible; display: none;"> <h1 style="color:#7600ff">DEC3PTIXNS HXB</h1>' +
 
-    '<h1 style="color: rgb(118 0 255); font-size: 18px">GET DAGGERS<h1>' +
-    '<div class="button" id="exploits_getDaggers_specific_btn" style="color:#7600ff; background:#000000; font-size:15px" padding="30px 15px">SPECIFIC</div>' +
-    '<div class="button" id="exploits_getDaggers_rapidSpecific_btn" style="color:#7600ff; background:#000000; font-size:15px">{RAPID} SPECIFIC</div>' +
-    '<div class="button" id="exploits_getDaggers_instaLevel_btn" style="color:#7600ff; background:#000000; font-size:15px">LEVEL</div>' +
-    '<div class="button" id="exploits_getDaggers_rapidLevel_btn" style="color:#7600ff; background:#000000; font-size:15px">{RAPID} LEVEL</div>' +
+    '<h1>GET DAGGERS<h1>' +
+    '<div class="button" id="exploits_getDaggers_specific_btn">SPECIFIC</div>' +
+    '<div class="button" id="exploits_getDaggers_rapidSpecific_btn" >{RAPID} SPECIFIC</div>' +
+    '<div class="button" id="exploits_getDaggers_instaLevel_btn" >LEVEL</div>' +
+    '<div class="button" id="exploits_getDaggers_rapidLevel_btn" >{RAPID} LEVEL</div>' +
 
-    '<h1 style="color: rgb(118 0 255); font-size: 18px">EQUIP DAGGERS<h1>' +
-    '<div class="button" id="exploits_equipDaggers_equipDagger_btn" style="color:#7600ff; background:#000000; font-size:15px">DAGGER</div>' +
-    '<div class="button" id="exploits_equipDaggers_equipLoadout_btn" style="color:#7600ff; background:#000000; font-size:15px">LOADOUT</div>' +
-    '<div class="button" id="exploits_equipDaggers_equipRandom_btn" style="color:#7600ff; background:#000000; font-size:15px">RANDOM</div>' +
-    '<div class="button" id="exploits_equipDaggers_equipUnobtainable_btn" style="color:#7600ff; background:#000000; font-size:15px">UNOBTAINABLE</div>' +
+    '<h1>EQUIP DAGGERS<h1>' +
+    '<div class="button" id="exploits_equipDaggers_equipDagger_btn" >DAGGER</div>' +
+    '<div class="button" id="exploits_equipDaggers_equipLoadout_btn" >LOADOUT</div>' +
+    '<div class="button" id="exploits_equipDaggers_equipRandom_btn" >RANDOM</div>' +
+    '<div class="button" id="exploits_equipDaggers_equipUnobtainable_btn" >UNOBTAINABLE</div>' +
 
-    '<h1 style="color: rgb(118 0 255); font-size: 18px">UPGRADES<h1>' +
-    '<div class="button" id="exploits_upgrades_infUpgrades_btn" style="color:#7600ff; background:#000000; font-size:15px">INFINITE</div>' +
-    '<div class="button" id="exploits_upgrades_freeUpgrades_btn" style="color:#7600ff; background:#000000; font-size:15px">FREE</div>' +
+    '<h1>UPGRADES<h1>' +
+    '<div class="button" id="exploits_upgrades_infUpgrades_btn" >INFINITE</div>' +
+    '<div class="button" id="exploits_upgrades_freeUpgrades_btn" >FREE</div>' +
 
-    '<h1 style="color: rgb(118 0 255); font-size: 18px">XP<h1>' +
-    '<div class="button" id="exploits_xp_set_btn" style="color:#7600ff; background:#000000; font-size:15px">SET</div>' +
+    '<h1>XP<h1>' +
+    '<div class="button" id="exploits_xp_set_btn" >SET</div>' +
 
-    '<h1 style="color: rgb(118 0 255); font-size: 18px">DAILY GIFTS<h1>' +
-    '<div class="button" id="exploits_dailyGift_instaGift_btn" style="color:#7600ff; background:#000000; font-size:15px">INSTANT</div>' +
-    '<div class="button" id="exploits_dailyGift_rapidGift_btn" style="color:#7600ff; background:#000000; font-size:15px">RAPID</div>' +
+    '<h1>DAILY GIFTS<h1>' +
+    '<div class="button" id="exploits_dailyGift_instaGift_btn" >INSTANT</div>' +
+    '<div class="button" id="exploits_dailyGift_rapidGift_btn" >RAPID</div>' +
 
-    '<h1 style="color: rgb(118 0 255); font-size: 18px">LEVELS<h1>' +
-    '<div class="button" id="exploits_levels_play_btn" style="color:#7600ff; background:#000000; font-size:15px">PLAY</div>' +
-    '<div class="button" id="exploits_levels_complete_btn" style="color:#7600ff; background:#000000; font-size:15px">COMPLETE</div>' +
-    '<div class="button" id="exploits_levels_unlock_btn" style="color:#7600ff; background:#000000; font-size:15px">UNLOCK</div>' +
+    '<h1>LEVELS<h1>' +
+    '<div class="button" id="exploits_levels_play_btn" >PLAY</div>' +
+    '<div class="button" id="exploits_levels_complete_btn" >COMPLETE</div>' +
+    '<div class="button" id="exploits_levels_unlock_btn" >UNLOCK</div>' +
 
     '</div>');
 
